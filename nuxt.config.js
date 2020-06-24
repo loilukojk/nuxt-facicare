e1b92e776a001b42afbb2fb4b2b82aeee85056cc
loilukojk <loinguyen.tan98@gmail.com>
2020-06-24 15:54:43 +0700

    test my nuxt-app

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,148 +1,84 @@
-const builtAt = new Date().toISOString()
-const path = require('path')
-const { I18N } = require('./locales/i18n-nuxt-config')
-import fs from 'fs'
-import Mode from "frontmatter-markdown-loader/mode"
-import MarkdownIt from 'markdown-it'
-import mip from 'markdown-it-prism'
+import colors from 'vuetify/es5/util/colors'
 
-function getPaths (lang, type) {
-  let initial = lang
-  if (lang === 'en') { initial = '' }
-  return fs.readdirSync(path.resolve(__dirname, 'contents', `${lang}/${type}`))
-    .filter(filename => path.extname(filename) === '.md')
-    .map(filename => `${initial}/${type}/${path.parse(filename).name}`)
-}
-
-const md = new MarkdownIt({
-  html: true,
-  typographer: true
-})
-md.use(mip)
-
-const productionUrl = {
-  en: "/en",
-  es: "/es"
-};
-const baseUrl = 'https://marinaaisa.com';
-
-module.exports = {
-  env: {
-    baseUrl,
-    productionUrl
-  },
+export default {
+  /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
+  mode: 'spa',
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'static',
+  /*
+  ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
+  */
   head: {
-    title: 'Marina Aisa | Product Designer & Front-end Developer',
+    titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no' },
-      { name: 'msapplication-TileColor', content: '#ffffff' },
-      { name: 'msapplication-TileImage', content: '/favicons/mstile-144x144.png' },
-      { name: 'theme-color', content: '#c1c1c1' },
-      { name: 'robots', content: 'index, follow' },
-      { name: 'twitter:card', content: 'summary_large_image' },
-      { name: 'twitter:site', content: '@marinaaisa' },
-      { property: 'og:type', content: 'profile' },
-      { property: 'og:updated_time', content: builtAt }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/png', href: '/favicons/favicon-16x16.png', sizes: '16x16' },
-      { rel: 'icon', type: 'image/png', href: '/favicons/favicon-32x32.png', sizes: '32x32' },
-      { rel: 'icon', type: 'image/png', href: '/favicons/android-chrome-96x96.png', sizes: '96x96' },
-      { rel: 'icon', type: 'image/png', href: '/favicons/android-chrome-192x192.png', sizes: '192x192' },
-      { rel: 'apple-touch-icon', href: '/favicons/apple-touch-icon-57x57.png', sizes: '57x57' },
-      { rel: 'apple-touch-icon', href: '/favicons/apple-touch-icon-60x60.png', sizes: '60x60' },
-      { rel: 'apple-touch-icon', href: '/favicons/apple-touch-icon-72x72.png', sizes: '72x72' },
-      { rel: 'apple-touch-icon', href: '/favicons/apple-touch-icon-76x76.png', sizes: '76x76' },
-      { rel: 'apple-touch-icon', href: '/favicons/apple-touch-icon-114x114.png', sizes: '114x114' },
-      { rel: 'apple-touch-icon', href: '/favicons/apple-touch-icon-120x120.png', sizes: '120x120' },
-      { rel: 'apple-touch-icon', href: '/favicons/apple-touch-icon-144x144.png', sizes: '144x144' },
-      { rel: 'apple-touch-icon', href: '/favicons/apple-touch-icon-152x152.png', sizes: '152x152' },
-      { rel: 'apple-touch-icon', href: '/favicons/apple-touch-icon-180x180.png', sizes: '180x180' },
-      { rel: 'mask-icon', type: 'image/png', href: '/favicons/safari-pinned-tab.svg', color: '#c1c1c1' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
   /*
-  ** Customize the progress-bar color
+  ** Global CSS
   */
-  loading: {
-    color: '#5a46ff',
-    height: '3px'
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  ** https://nuxtjs.org/guide/plugins
+  */
+  plugins: [
+  ],
+  /*
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
+  */
+  components: true,
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    '@nuxtjs/vuetify',
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+  /*
+  ** vuetify module configuration
+  ** https://github.com/nuxt-community/vuetify-module
+  */
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
   },
   /*
   ** Build configuration
+  ** See https://nuxtjs.org/api/configuration-build/
   */
-  css: [
-    'normalize.css/normalize.css',
-    '@/assets/css/main.scss',
-    '@/assets/css/prism-material-light.css'
-  ],
-
   build: {
-    extend (config) {
-      const rule = config.module.rules.find(r => r.test.toString() === '/\\.(png|jpe?g|gif|svg|webp)$/i')
-      config.module.rules.splice(config.module.rules.indexOf(rule), 1)
-
-      config.module.rules.push({
-        test: /\.md$/,
-        loader: 'frontmatter-markdown-loader',
-        include: path.resolve(__dirname, 'contents'),
-        options: {
-          mode: [Mode.VUE_RENDER_FUNCTIONS, Mode.VUE_COMPONENT],
-          vue: {
-            root: "dynamicMarkdown"
-          },
-          markdown(body) {
-            return md.render(body)
-          }
-        }
-      }, {
-        test: /\.(jpe?g|png)$/i,
-        loader: 'responsive-loader',
-        options: {
-          placeholder: true,
-          quality: 60,
-          size: 1400,
-          adapter: require('responsive-loader/sharp')
-        }
-      }, {
-        test: /\.(gif|svg)$/,
-        loader: 'url-loader',
-        query: {
-          limit: 1000,
-          name: 'img/[name].[hash:7].[ext]'
-        }
-      });
-    }
-  },
-  plugins: ['~/plugins/lazyload', '~/plugins/globalComponents', { src: '~plugins/ga.js', ssr: false }],
-  modules: [  
-    '@nuxtjs/style-resources',
-    ['nuxt-i18n', I18N],
-    'nuxt-webfontloader'
-  ],
-
-  styleResources: {
-    scss: [
-      '@/assets/css/utilities/_variables.scss',
-      '@/assets/css/utilities/_helpers.scss',
-      '@/assets/css/base/_grid.scss',
-      '@/assets/css/base/_buttons.scss'
-    ],
-  },
-
-  webfontloader: {
-    custom: {
-      families: ['Graphik', 'Tiempos Headline'],
-      urls: ['/fonts/fonts.css']
-    }
-  },
-
-  generate: {
-    routes: [
-      '/es', '404'
-    ]
-    .concat(getPaths('es', 'blog'))
-    .concat(getPaths('en', 'blog'))
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,97 @@
 <template>
-  <div class="page-index">
-    <div class="container">
-      <BlogSection :blogs="blogs"/>
-    </div>
-  </div>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-center">
+        <logo />
+        <vuetify-logo />
+      </div>
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to the Vuetify + Nuxt.js template
+        </v-card-title>
+        <v-card-text>
+          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
+          <p>
+            For more information on Vuetify, check out the <a
+              href="https://vuetifyjs.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              documentation
+            </a>.
+          </p>
+          <p>
+            If you have questions, please join the official <a
+              href="https://chat.vuetifyjs.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="chat"
+            >
+              discord
+            </a>.
+          </p>
+          <p>
+            Find a bug? Report it on the github <a
+              href="https://github.com/vuetifyjs/vuetify/issues"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="contribute"
+            >
+              issue board
+            </a>.
+          </p>
+          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
+          <div class="text-xs-right">
+            <em><small>&mdash; John Leider</small></em>
+          </div>
+          <hr class="my-3">
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nuxt Documentation
+          </a>
+          <br>
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nuxt GitHub
+          </a>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to="/inspire"
+          >
+            Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-  import BlogSection from "~/components/Sections/BlogSection"
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
-  import blogsEn from '~/contents/en/blogsEn.js'
-  import blogsEs from '~/contents/es/blogsEs.js'
-
-  export default {
-    async asyncData ({app}) {
-
-      const blogs = app.i18n.locale === 'en' ? blogsEn : blogsEs
-      
-      async function asyncImport (blogName) {
-        const wholeMD = await import(`~/contents/${app.i18n.locale}/blog/${blogName}.md`)
-        return wholeMD.attributes
-      }
-
-      return Promise.all(blogs.map(blog => asyncImport(blog)))
-      .then((res) => {
-        return {
-          blogs: res
-        }
-      })
-    },
-    
-    components: { BlogSection },
-
-    transition: {
-      name: 'slide-fade'
-    },
-
-    head () {
-      return {
-        title: this.$t('indexPageHead.title'),
-        htmlAttrs: {
-          lang: this.$i18n.locale,
-        },
-        script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
-        meta: [
-          { name: "author", content: "Marina Aisa" },
-          { name: "description", property: "og:description", content: this.$t('indexPageHead.description'), hid: "description" },
-          { property: "og:title", content: this.$t('indexPageHead.title') },
-          { property: "og:image", content: this.ogImage },
-          { name: "twitter:description", content: this.$t('indexPageHead.description') },
-          { name: "twitter:image", content: this.ogImage }
-        ]
-      };
-    },
-
-    computed: {
-      ogImage: function () {
-        return;
-      }
-    }
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
   }
+}
 </script>
